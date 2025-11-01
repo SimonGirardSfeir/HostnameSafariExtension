de66a3adb0839ab3a7f3058648c0aac1c5c5a848
Simon Girard <simon.girard1991@gmail.com>
2025-11-01 15:35:03 +0100

    Initial Commit

---
+++ FirstExtension/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FirstExtension
+//
+//  Created by Simon Girard on 01.11.2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ FirstExtensionTests/FirstExtensionTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstExtensionTests.swift
+//  FirstExtensionTests
+//
+//  Created by Simon Girard on 01.11.2025.
+//
+
+import Testing
+@testable import FirstExtension
+
+struct FirstExtensionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
